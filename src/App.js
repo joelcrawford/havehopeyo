0ba466172167dc8f4bca2e8160021019855628c6
joelcrawford <joelrootcrawford@gmail.com>
2019-07-06 16:09:06 -0700

    initial css, learning flex

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header className="header">
+        <div className="content">
+          <h1>Have Hope Yo</h1>
+          <p>Pellentesque habitant morbi tristique senectus et netus et 
+          malesuada fames ac turpis egestas. Vestibulum tortor quam, 
+          feugiat vitae, ultricies eget</p>
+        </div>
       </header>
+      <section className="banner"></section>
+      <section className="col-primary">
+        <p>Pellentesque habitant morbi tristique senectus et netus et 
+          malesuada fames ac turpis egestas. Vestibulum tortor quam, 
+          feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu 
+          libero sit amet quam egestas semper. Aenean ultricies mi vitae est. 
+          Mauris placerat eleifend leo.</p> 
+          <img src={logo} alt="logo" />
+      </section>
+      <section className="aside col-secondary">
+        <p>Pellentesque habitant morbi tristique senectus et netus et 
+            malesuada fames ac turpis egestas. Vestibulum tortor quam, 
+            feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu 
+            libero sit amet quam egestas semper. Aenean ultricies mi vitae est. 
+            Mauris placerat eleifend leo.</p> 
+      </section>
+      <footer className="footer">
+        <p>Pellentesque habitant morbi tristique senectus et netus et 
+              malesuada fames ac turpis egestas. Vestibulum tortor quam, 
+              feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu 
+              libero sit amet quam egestas semper. Aenean ultricies mi vitae est. 
+              Mauris placerat eleifend leo.</p> 
+      </footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
